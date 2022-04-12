41c95ea611720fb9af7e15503d5efe7207676c45
3badymek <aboooy.aa@gmail.com>
2022-04-12 18:23:51 +0300

    First Commit

---
+++ wifiqr.py
@@ -0,0 +1,28 @@
+import pyqrcode
+import sys
+import base64
+
+### requirement packages ##
+# pip install pyqrcode
+# pip install pypng
+###########################
+
+#if no output provided
+if len(sys.argv) < 2:
+    print ("Usage: pyton3 wifiqr.py [network SSID] [password]")
+    quit()
+
+#set network SSID name
+network = sys.argv[1]
+
+#set the value of WIFI network
+if len(sys.argv) > 2:
+    protocol = "WPA/WPA2"
+    pwd = sys.argv[2]
+else:
+    protocol = "nopass"
+    pwd = ""
+
+qr = pyqrcode.create(f"WIFI:S:{network};T:{protocol};P:{pwd};;")
+
+png = qr.png(f"{network}.png", scale=5)
